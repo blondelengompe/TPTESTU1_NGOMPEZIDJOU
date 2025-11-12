f0365bdff83b8e48fb3d000cf1f474b530d5a3fd
unknown <blondele.ngompe@institutsaintjean.org>
2025-11-12 01:29:25 +0100

    Initialisation du projet Maven

---
+++ TPTESTU1_NGOMPEZIDJOU/src/main/java/com/stjean/operation/TPTESTU1_NGOMPEZIDJOU/Tptestu1NgompezidjouApplication.java
@@ -0,0 +1,13 @@
+package com.stjean.operation.TPTESTU1_NGOMPEZIDJOU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tptestu1NgompezidjouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tptestu1NgompezidjouApplication.class, args);
+	}
+
+}
---
+++ TPTESTU1_NGOMPEZIDJOU/src/test/java/com/stjean/operation/TPTESTU1_NGOMPEZIDJOU/Tptestu1NgompezidjouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stjean.operation.TPTESTU1_NGOMPEZIDJOU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tptestu1NgompezidjouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
